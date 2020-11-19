48489227b5b16c810771eecee05ff26d870bbc42
Zhian N. Kamvar <zkamvar@gmail.com>
2020-11-18 16:13:10 -0800

    add more tests

--- tests/testthat/test-get_dropdown.R
+++ tests/testthat/test-get_dropdown.R
@@ -8,6 +8,10 @@ res <- create_lesson(tmp)
 create_episode("outroduction", path = res)
 outro <- fs::path(res, "episodes", "02-outroduction.Rmd")
 fs::file_move(outro, fs::path_ext_set(outro, "md"))
+# NOTE: make sure that filenames do not clash at the moment... they will. 
+lt <- fs::file_create(fs::path(tmp, "learners", c("learner-test.md")))
+it <- fs::file_create(fs::path(tmp, "instructors", c("test1.md", "test2.md")))
+pt <- fs::file_create(fs::path(tmp, "profiles", c("profileA.md", "profileB.md")))
 eps <- c("01-introduction.Rmd", "02-outroduction.md")
 
 test_that("get_dropdown works as expected", {
@@ -39,4 +43,51 @@ test_that("get_episodes() works in the right order", {
 
 })
 
+test_that("get_learners() works as expected", {
+
+  expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "learners"))))
+  expect_equal(expected, c("Setup.md", basename(lt)))
+
+  expect_silent(l <- get_learners(res))
+  expect_equal(l, expected)
+  set_learners(res, rev(l), write = TRUE)
+  expect_silent(l <- get_learners(res))
+  expect_equal(l, rev(expected))
+  expect_silent(l <- get_learners(res, trim = FALSE))
+  expect_equal(basename(l), rev(expected))
+  expect_failure(expect_equal(l, rev(expected)))
+
+})
+
+test_that("get_instructors() works as expected", {
+
+  expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "instructors"))))
+  expect_equal(basename(it), expected)
+
+  expect_silent(i <- get_instructors(res))
+  expect_equal(i, expected)
+  set_instructors(res, rev(i), write = TRUE)
+  expect_silent(i <- get_instructors(res))
+  expect_equal(i, rev(expected))
+  expect_silent(i <- get_instructors(res, trim = FALSE))
+  expect_equal(basename(i), rev(expected))
+  expect_failure(expect_equal(i, rev(expected)))
+
+})
+
+test_that("get_profiles() works as expected", {
+
+  expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "profiles"))))
+  expect_equal(basename(pt), expected)
+  
+  expect_silent(p <- get_profiles(res))
+  expect_equal(p, expected)
+  set_profiles(res, rev(p), write = TRUE)
+  expect_silent(p <- get_profiles(res))
+  expect_equal(p, rev(expected))
+  expect_silent(p <- get_profiles(res, trim = FALSE))
+  expect_equal(basename(p), rev(expected))
+  expect_failure(expect_equal(p, rev(expected)))
+
+})
 
