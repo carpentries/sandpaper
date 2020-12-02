8400171ad502a70ddf727243f4b0193e3e67d11b
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-01 17:05:41 -0800

    add ci_build_site for testing

--- R/ci_build_markdown.R
+++ R/ci_build_markdown.R
@@ -49,3 +49,38 @@ ci_build_markdown <- function(path = ".", branch = "md-outputs", remote = "origi
     remote, branch
   )
 }
+
+ci_build_site <- function(path = ".", branch = "gh-pages", md = "md-outputs", remote = "origin") {
+
+  # step 0: build_lesson defaults to a local build
+  path <- set_source_path(path)
+  on.exit(reset_build_paths())
+
+  create_site(path)
+
+  built <- path_built(path)
+  html  <- fs::path(path_site(path), "docs")
+
+  # Set up the worktrees and make sure to remove them when the function exits
+  # (gracefully or ungracefully so)
+  # ------------ markdown worktree
+  del_md <- git_worktree_setup(path, built, 
+    branch = md, remote = remote
+  )
+  on.exit(eval(del_md), add = TRUE)
+
+  # ------------ site worktree
+  del_site <- git_worktree_setup(path, html,
+    branch = branch, remote = remote
+  )
+
+  on.exit(eval(del_site), add = TRUE)
+
+  # Will not rebuild the files that were already built
+  build_site(path = path, quiet = TRUE, rebuild = FALSE)
+
+  github_worktree_commit(html,
+    "site deploy",
+    remote, branch
+  )
+}
