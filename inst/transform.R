e657cedab0ab6d42fa90cb6d0b23f26d349991cc
Zhian N. Kamvar <zkamvar@gmail.com>
2021-04-27 16:29:00 -0700

    update transformer

--- inst/transform.R
+++ inst/transform.R
@@ -7,16 +7,24 @@ pgap <- pegboard::Lesson$new(src)
 # Script to transform the episodes via pegboard with traces
 transform <- function(e, out = src) {
   outdir <- fs::path(out, "episodes/")
-  message(glue::glue("{e$name}: converting blockquotes to fenced div"))
+  cli::cat_rule(fs::path_rel(e$name, out)) # -----------------------------------
+  cli::cli_process_start(glue::glue(" converting blockquotes to fenced div"))
   e$unblock()
-  message(glue::glue("{e$name}: removing Jekyll syntax"))
+  cli::cli_process_done()
+
+  cli::cli_process_start(glue::glue("removing Jekyll syntax"))
   e$use_sandpaper()
-  message(glue::glue("{e$name}: moving yaml items to body"))
+  cli::cli_process_done()
+
+  cli::cli_process_start(glue::glue("moving yaml items to body"))
   e$move_questions()
   e$move_objectives()
   e$move_keypoints()
-  message(glue::glue("{e$name}: writing output"))
+  cli::cli_process_done()
+
+  cli::cli_process_start(glue::glue("writing output"))
   e$write(outdir, format = "Rmd", edit = FALSE)
+  cli::cli_process_done()
 }
 
 # function to remove directories
@@ -27,13 +35,13 @@ dirdel <- Vectorize({function(d) {
 
 # Read and and transform additional files
 rewrite <- function(x, out) {
-  ref <- Episode$new()
+  ref <- Episode$new(x)
   ref$unblock()$use_sandpaper()$write(out)
 }
 
 set_config <- function(key, value, path = here()) {
   sandpaper::set_dropdown(path, 
-    order = value
+    order = value,
     write = TRUE,
     folder = key
   ) 
@@ -41,7 +49,8 @@ set_config <- function(key, value, path = here()) {
 
 # Create a branch for us to work in 
 current_branch <- git_branch()
-message(glue::glue("switching from {current_branch} to sandpaper")
+message(glue::glue("switching from {current_branch} to sandpaper"))
+if (git_branch_exists("sandpaper")) git_branch_delete("sandpaper")
 git_branch_create("sandpaper", ref = "HEAD", checkout = TRUE)
 
 # Create a lesson template and remove the git repository
@@ -51,31 +60,35 @@ sandpaper::create_lesson(tmp, name = cfg$title, open = FALSE)
 fs::dir_delete(fs::path(tmp, ".git"))
 fs::file_delete(fs::path(tmp, c("README.md", "index.md", ".gitignore")))
 fs::file_delete(fs::path(tmp, "episodes", "01-introduction.Rmd"))
-fs::file_delete(fs::path(tmp, "episodes", "01-introduction.Rmd"))
-fs::file_delete(fs::path(tmp, "learners", "Setup.md") 
-fs::file_delete(fs::path(tmp, "instructors", "instructor-notes.md")
+fs::file_delete(fs::path(tmp, "learners", "Setup.md"))
+fs::file_delete(fs::path(tmp, "instructors", "instructor-notes.md"))
 
 # Copy the components here
+message(glue::glue("copying the sandpaper template on top of the current files"))
 fs::dir_copy(tmp, here(), overwrite = TRUE)
 
 # Modify config file to match as close as possible to the one we have
+message("setting the configuration parameters in config.yaml")
+set_config("title", cfg$title)
 set_config("life_cycle", cfg$life_cycle)
 set_config("contact", cfg$email)
 set_config("branch", current_branch)
 
-if (is.null(gh::gh_whoami()) {
+if (length(gert::git_remote_list()) == 0) {
   message("Cannot automatically set the following configuration values:\n source: <GITHUB URL>\n carpentry: <CARPENTRY ABBREVIATION>\n\nPlease edit config.yaml to set these values")
 } else {
-  rmt <- gh::gh_tree_remote()
-  url <- glue::glue("https://github.com/{glue::glue_collapse(rmt, '/')}/")
+  rmt <- gert::git_remote_list()
+  i <- if (any(i <- rmt$name == "upstream")) which(i) else 1L
+  url <- rmt$url[[i]]
+  rmt <- gh:::github_remote_parse(rmt$url[[i]])$username
   set_config("source", url)
   set_config("carpentry", 
     switch(rmt, 
       swcarpentry = "swc",
       datacarpentry = "dc",
       librarycarpentry = "lc",
-      "carpentries-incubator" = "incubator"
-      "cp", # default
+      "carpentries-incubator" = "incubator",
+      "cp" # default
   ))  
 }
 
@@ -85,6 +98,8 @@ purrr::walk(pgap$episodes, ~try(transform(.x)))
 
 # Transform non-episode MD files
 rewrite(here("_extras", "design.md"), here("instructors"))
+# NOTE: quotation is throwing things off here :(
+
 rewrite(here("_extras", "guide.md"), here("instructors"))
 
 rewrite(here("_extras", "discuss.md"), here("learners"))
