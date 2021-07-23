96eb907300ccfcd9a43a7282a10ceb66175d696a
Zhian N. Kamvar <zkamvar@gmail.com>
2021-07-23 14:39:36 -0700

    add tests and snaps for get_dropdown

---
+++ tests/testthat/_snaps/get_dropdown.md
@@ -0,0 +1,23 @@
+# get_dropdown works as expected with messages [plain]
+
+    Code
+      s <- get_dropdown(res, "episodes")
+    Message <cliMessage>
+      i No schedule set, using Rmd files in 'episodes/' directory.
+      > To remove this message, define your schedule in 'config.yaml' or use `set_episodes()` to generate it.
+
+# get_dropdown works as expected with messages [ansi]
+
+    Code
+      s <- get_dropdown(res, "episodes")
+
+# get_dropdown works as expected with messages [unicode]
+
+    Code
+      s <- get_dropdown(res, "episodes")
+
+# get_dropdown works as expected with messages [fancy]
+
+    Code
+      s <- get_dropdown(res, "episodes")
+
--- tests/testthat/test-get_dropdown.R
+++ tests/testthat/test-get_dropdown.R
@@ -1,7 +1,7 @@
+{
 tmpdir <- fs::file_temp()
 fs::dir_create(tmpdir)
 tmp    <- fs::path(tmpdir, "lesson-example")
-
 withr::defer(fs::dir_delete(tmp))
 expect_false(fs::dir_exists(tmp))
 res <- create_lesson(tmp, open = FALSE)
@@ -14,18 +14,22 @@ lt <- fs::file_create(fs::path(tmp, "learners", c("learner-test.md")))
 it <- fs::file_create(fs::path(tmp, "instructors", c("test1.md", "test2.md")))
 pt <- fs::file_create(fs::path(tmp, "profiles", c("profileA.md", "profileB.md")))
 eps <- c("01-introduction.Rmd", "02-outroduction.md")
+}
 
-test_that("get_dropdown works as expected", {
+cli::test_that_cli("get_dropdown works as expected with messages", {
   
   expect_error(get_dropdown(res), "folder") # folder missing with no default 
-  expect_warning(s <- get_dropdown(res, "episodes"), "set_episodes()", fixed = TRUE)
+  expect_snapshot(s <- get_dropdown(res, "episodes"))
+  expect_equal(s, eps)
+  set_episodes(res, s, write = TRUE)
+  expect_silent(s <- get_episodes(res))
   expect_equal(s, eps)
 
 })
 
 test_that("get_episodes() works with trim = FALSE", {
 
-  expect_warning(s <- get_episodes(res, trim = FALSE), "set_episodes()", fixed = TRUE)
+  expect_silent(s <- get_episodes(res, trim = FALSE))
   expect_equal(basename(s), eps)
   expect_failure(expect_equal(s, eps))
 
@@ -33,18 +37,17 @@ test_that("get_episodes() works with trim = FALSE", {
 
 test_that("get_episodes() works in the right order", {
 
-
-  expect_warning(s <- get_episodes(res), "set_episodes()", fixed = TRUE)
+  expect_silent(s <- get_episodes(res))
   set_episodes(res, rev(s), write = TRUE)
   expect_equal(get_episodes(res), rev(eps))
-  s <- get_episodes(res, trim = FALSE)
+
+  expect_silent(s <- get_episodes(res, trim = FALSE))
   expect_equal(basename(s), rev(eps))
   expect_failure(expect_equal(s, rev(eps)))
-  
 
 })
 
-test_that("get_learners() works as expected", {
+test_that("get_learners() returns contents of the learners directory", {
 
   expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "learners"))))
   expect_setequal(expected, c("Setup.md", basename(lt)))
@@ -60,7 +63,7 @@ test_that("get_learners() works as expected", {
 
 })
 
-test_that("get_instructors() works as expected", {
+test_that("get_instructors() returns the contents of the instructors directory", {
 
   expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "instructors"))))
   expect_equal(c("instructor-notes.md", basename(it)), expected)
@@ -76,7 +79,7 @@ test_that("get_instructors() works as expected", {
 
 })
 
-test_that("get_profiles() works as expected", {
+test_that("get_profiles() returns the contents of the profiles directory", {
 
   expected <- basename(as.character(fs::dir_ls(fs::path(tmp, "profiles"))))
   expect_equal(c("learner-profiles.md", basename(pt)), expected)
