17080aa2bd1b5a06f7e73c08acd1ddb48f46467d
Zhian N. Kamvar <zkamvar@gmail.com>
2021-10-21 12:04:42 -0700

    add test to confirm the fix for colons
    This will fix #192

--- tests/testthat/test-template-funs.R
+++ tests/testthat/test-template-funs.R
@@ -5,19 +5,3 @@ test_that("template files point to the right places", {
   expect_equal(fs::path_file(template_links()), "links-template.txt")
 })
 
-test_that("template_pkgdown() protects string values", {
-
-  yaml <- get_yaml_text(template_pkgdown())
-  # This would normally throw an error
-  yaml <- whisker::whisker.render(yaml, data = list(title = "a: b"))
-  expect_match(yaml, "title: 'a: b'")
-
-})
-
-test_that("template_config() protects string values", {
-  
-  yaml <- get_yaml_text(template_config())
-  yaml <- whisker::whisker.render(yaml, data = list(title = "a: b"))
-  expect_match(yaml, "title: 'a: b'")
-
-})
