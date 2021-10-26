2db352b06161c4994c52f485851dd08b100a8d5a
Zhian N. Kamvar <zkamvar@gmail.com>
2021-10-26 16:24:57 -0700

    move inline markdown to example file

--- tests/testthat/test-render_html.R
+++ tests/testthat/test-render_html.R
@@ -1,59 +1,4 @@
-ex <- c("---",
-  "teaching: 6",
-  "exercises: 9",
-  "---",
-  "",
-  "::: questions", 
-  "", 
-  " - What's the point?",
-  "", 
-  ":::",
-  "",
-  "::: objectives", 
-  "", 
-  " - Bake him away, toys",
-  "", 
-  ":::",
-  "",
-  "# Markdown", 
-  "", 
-  "::: challenge", 
-  "", 
-  "How do you write markdown divs?",
-  "", 
-  "This [link should be transformed](../learners/Setup.md)",
-  "", 
-  "This [rmd link also](../episodes/01-Introduction.Rmd)",
-  "",
-  "This [rmd is safe](https://example.com/01-Introduction.Rmd)",
-  "",
-  "This [too](../learners/Setup.md#windows-setup 'windows setup')",
-  "",
-  "![link should be transformed](../episodes/fig/Setup.png){alt='alt text'}",
-  "", 
-  "::: solution",
-  "",
-  "# Write now",
-  "",
-  "just write it, silly.",
-  ":::",
-  ":::",
-  "", 
-  "::: instructor", 
-  "", 
-  "This should be aside",
-  "", 
-  ":::",
-  "", 
-  "::: nothing", 
-  "", 
-  "This should be",
-  "", 
-  ":::",
-  NULL
-)
-
-
+example_markdown <- fs::path_abs(test_path("examples", "ex.md"))
 
 test_that("emoji are rendered", {
   skip_if_not(rmarkdown::pandoc_available("2.11"))
@@ -99,12 +44,10 @@ test_that("footnotes are rendered", {
 test_that("pandoc structure is rendered correctly", {
   
   skip_if_not(rmarkdown::pandoc_available("2.11"))
-  tmp <- fs::file_temp()
   out <- fs::file_temp()
-  withr::local_file(tmp, out)
+  withr::local_file(out)
 
-  writeLines(ex, tmp)
-  args <- construct_pandoc_args(tmp, out, to = "native")
+  args <- construct_pandoc_args(example_markdown, out, to = "native")
   callr::r(function(...) rmarkdown::pandoc_convert(...), args = args)
   if (.Platform$OS.type == "windows" && Sys.getenv("CI", unset = '') == "true") {
     # let's see what this looks like
@@ -121,6 +64,7 @@ test_that("paragraphs after objectives block are parsed correctly", {
   out <- fs::file_temp()
   withr::local_file(tmp, out)
 
+  ex <- readLines(example_markdown)
   ex2 <- c(ex[1:16], "", "Do you think he saurus?", ex[17:18])
   writeLines(ex2, tmp)
   args <- construct_pandoc_args(tmp, out, to = "native")
@@ -138,11 +82,7 @@ test_that("render_html applies the internal lua filter", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
 
-  tmp <- fs::file_temp()
-  withr::local_file(tmp)
-
-  writeLines(ex, tmp)
-  res <- render_html(tmp)
+  res <- render_html(example_markdown)
 
   # Metadata blocks are parsed
   expect_match(res, "div class=\"row\"", fixed = TRUE)
@@ -177,18 +117,16 @@ test_that("render_html applies the internal lua filter", {
 test_that("render_html applies external lua filters", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
-  tmp <- fs::file_temp()
   lua <- fs::file_temp()
-  withr::local_file(tmp, lua)
+  withr::local_file(lua)
 
-  writeLines(ex, tmp)
   lu <- c("Str = function (elem)",
     "  if elem.text == 'markdown' then",
     "    return pandoc.Emph {pandoc.Str 'mowdrank'}",
     "  end",
     "end")
   writeLines(lu, lua)
-  res <- render_html(tmp, paste0("--lua-filter=", lua))
+  res <- render_html(example_markdown, paste0("--lua-filter=", lua))
   expect_match(res, "<em>mowdrank</em> divs", fixed = TRUE)
   
 })
