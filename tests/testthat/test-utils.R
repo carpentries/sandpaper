02178f3c7ce19448b5ab1894b8fd26e0340dba6b
Zhian N. Kamvar <zkamvar@gmail.com>
2021-11-22 11:17:35 -0800

    add test for copy_assets

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,3 +4,13 @@ test_that("null pipe works", {
   expect_equal(NA_character_ %||% LETTERS, NA_character_)
 })
 
+
+test_that("copy assets will fail gracefully", {
+
+  tmpdir <- fs::file_temp()
+  withr::defer(fs::dir_delete(c(tmpdir)))
+  fs::dir_create(tmpdir)
+  expect_message(copy_assets(getOption("sandpaper.test_fixture"), td2), 
+    "There was an issue copying")
+
+})
