1cdb8ba6baa95a127394c6e67652743cdc4d5404
Zhian N. Kamvar <zkamvar@gmail.com>
2022-02-10 11:08:38 -0800

    attempt to modify tests

--- tests/testthat/test-build_home.R
+++ tests/testthat/test-build_home.R
@@ -5,11 +5,20 @@ test_that("build_home() works independently", {
   pkg <- pkgdown::as_pkgdown(path_site(res))
   expect_output(pkgdown::init_site(pkg))
   expect_false(fs::file_exists(fs::path(pkg$dst_path, "index.html")))
-  build_episode_md(fs::path(res, "index.md"), quiet = TRUE)
+  built_dir <- fs::path(pkg$src_path, "built")
+  fs::dir_create(built_dir)
+  fs::file_copy(fs::path(res, "index.md"), built_dir)
+  fs::file_copy(fs::path(res, "learners", "setup.md"), built_dir)
   build_home(pkg, quiet = FALSE, new_setup = TRUE, 
     next_page = fs::path(res, "episodes", "01-introduction.Rmd")
   )
-  expect_true(fs::file_exists(fs::path(pkg$dst_path, "index.html")))
-  expect_true(fs::file_exists(fs::path(pkg$dst_path, "instructor", "index.html")))
+  learn_index <- fs::path(pkg$dst_path, "index.html")
+  expect_true(fs::file_exists(learn_index))
+  html <- xml2::read_html(learn_index)
+
+  
+  
+  instruct_index <- fs::path(pkg$dst_path, "instructor", "index.html")
+  expect_true(fs::file_exists(instruct_index))
 })
 
