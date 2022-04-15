d835f0530d3ced6c4147594010ff8c06012e91e9
Zhian N. Kamvar <zkamvar@gmail.com>
2022-04-15 09:24:39 -0700

    add test

---
+++ tests/testthat/test-utils-aggregate.R
@@ -0,0 +1,18 @@
+
+test_that("read_all_html returns appropriate files", {
+
+  tmpdir <- withr::local_tempdir()
+  fs::dir_create(tmpdir)
+  fs::dir_create(fs::path(tmpdir, "instructor"))
+  writeLines("<p>Instructor</p>", fs::path(tmpdir, "instructor", "index.html"))
+  writeLines("<p>Learner</p>", fs::path(tmpdir, "index.html"))
+  res <- read_all_html(tmpdir)
+  expect_named(res, c( "instructor", "learner","paths"))
+  expect_length(res$paths, 2L)
+  expect_s3_class(res$learner$index, "xml_document")
+  expect_s3_class(res$instructor$index, "xml_document")
+  expect_equal(xml2::xml_text(res$learner$index), "Learner")
+  expect_equal(xml2::xml_text(res$instructor$index), "Instructor")
+
+})
+
