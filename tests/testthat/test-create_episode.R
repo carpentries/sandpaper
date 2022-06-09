8e8db8de29151bef48fa7d2aa93523f408818680
Zhian N. Kamvar <zkamvar@gmail.com>
2022-06-09 12:01:48 -0700

    add tests for create episode that slugs
    these address #294

--- tests/testthat/test-create_episode.R
+++ tests/testthat/test-create_episode.R
@@ -2,29 +2,27 @@ tmp <- res <- restore_fixture()
 
 test_that("prefixed episodes can be created", {
 
-  # Make sure everything exists
-  expect_true(check_lesson(tmp))
-
   initial_episode <- fs::dir_ls(fs::path(tmp, "episodes"), glob = "*Rmd") %>%
     expect_length(1L) %>%
     expect_match("01-introduction.Rmd")
 
-  initial_episode_md5 <- tools::md5sum(initial_episode)
-
-  second_episode <- create_episode("first-script", path = tmp) %>%
+  second_episode <- create_episode("First Script", path = tmp) %>%
     expect_match("02-first-script.Rmd", fixed = TRUE)
 
-  expect_equal(tools::md5sum(second_episode), initial_episode_md5, ignore_attr = TRUE)
+  expect_equal(readLines(second_episode, n = 2)[[2]], "title: 'First Script'")
+  expect_equal(readLines(initial_episode, n = 2)[[2]], "title: 'introduction'")
 
   expect_true(check_episode(initial_episode))
   expect_true(check_episode(second_episode))
- 
 
 })
 
 test_that("un-prefixed episodes can be created", {
-  third_episode <- create_episode("third-script", make_prefix = FALSE, path = tmp) %>%
-    expect_match("third-script.Rmd", fixed = TRUE)
+  title <- "\uC548\uB155 :joy_cat: \U0001F62D KITTY"
+  third_episode <- create_episode(title, make_prefix = FALSE, path = tmp) %>%
+    expect_match("\uC548\uB155-\U0001F62D-kitty.Rmd", fixed = TRUE)
 
   expect_true(check_episode(third_episode))
+  expect_equal(readLines(third_episode, n = 2)[[2]], 
+    paste0("title: '", title, "'"))
 })
