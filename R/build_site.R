be75ddb16cb451f48b5232ddc19073e7da6c87fa
Zhian N. Kamvar <zkamvar@gmail.com>
2022-06-22 15:47:17 -0700

    mask autolink in shim
    This will address #298

--- R/build_site.R
+++ R/build_site.R
@@ -61,8 +61,7 @@ build_site <- function(path = ".", quiet = !interactive(), preview = TRUE, overr
   sidebar <- create_sidebar(c(fs::path(built_path, "index.md"), chapters))
   # shim for downlit ----------------------------------------------------------
   shimstem_file <- system.file("pkgdown", "shim.R", package = "sandpaper")
-  # expected <- "5484c37e9b9c324361d775a10dea4946"
-  expected <- "8808c462f326fea560ed2d748ad459e5"
+  expected <- "b1d63f7db43e00e798a144743e4628b7"
   actual   <- tools::md5sum(shimstem_file)
   if (expected == actual) {
     # evaluate the shim in our namespace
--- inst/pkgdown/shim.R
+++ inst/pkgdown/shim.R
@@ -14,9 +14,17 @@
 dl <- asNamespace("downlit")
 tr <- dl$token_href
 ht <- dl$href_topic
+alc <- dl$autolink_curly
+alu <- dl$autolink_url
 unlockBinding("token_href", dl)
 unlockBinding("href_topic", dl)
+unlockBinding("autolink_curly", dl)
+unlockBinding("autolink_url", dl)
 dl$token_href <- function(token, text) rep(NA, length(token))
 dl$href_topic <- function(topic, package) NA_character_
+dl$autolink_curly <- function(text) NA_character_
+dl$autolink_url   <- function(text) NA_character_
 parse(text = "{\ndl$token_href <- tr\nlockBinding('token_href', dl)\n}")
 parse(text = "{\ndl$href_topic <- ht\nlockBinding('href_topic', dl)\n}")
+parse(text = "{\ndl$href_topic <- alc\nlockBinding('autolink_curly', dl)\n}")
+parse(text = "{\ndl$href_topic <- alu\nlockBinding('autolink_url', dl)\n}")
