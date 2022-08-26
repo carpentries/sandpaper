5d158c4d821516a9aefc4a9aaf0dd16c06b7491f
Zhian N. Kamvar <zkamvar@gmail.com>
2022-08-26 07:40:55 -0700

    update tests for build_lesson

--- tests/testthat/test-build_lesson.R
+++ tests/testthat/test-build_lesson.R
@@ -15,11 +15,11 @@ test_that("Lessons built for the first time are noisy", {
   })
   htmls <- read_all_html(sitepath)
   expect_setequal(names(htmls$learner), 
-    c("01-introduction", "index", "LICENSE", "CODE_OF_CONDUCT", "profiles", 
+    c("introduction", "index", "LICENSE", "CODE_OF_CONDUCT", "profiles", 
       "instructor-notes", "key-points", "aio", "images")
   )
   expect_setequal(names(htmls$instructor), 
-    c("01-introduction", "index", "LICENSE", "CODE_OF_CONDUCT", "profiles", 
+    c("introduction", "index", "LICENSE", "CODE_OF_CONDUCT", "profiles", 
       "instructor-notes", "key-points", "aio", "images")
   )
 
@@ -56,7 +56,7 @@ test_that("aio page can be rebuilt", {
   html <- xml2::read_html(aio)
   content <- get_content(html, "section[starts-with(@id, 'aio-')]")
   expect_length(content, 1L)
-  expect_equal(xml2::xml_attr(content, "id"), "aio-01-introduction")
+  expect_equal(xml2::xml_attr(content, "id"), "aio-introduction")
 
   # add an ephemeral section and write it out
   xml2::xml_add_sibling(content[[1]], "section", id = "ephemeral")
@@ -81,7 +81,7 @@ test_that("keypoints page can be rebuilt", {
   html <- xml2::read_html(keypoints)
   content <- get_content(html, "section")
   expect_length(content, 1L)
-  expect_equal(xml2::xml_attr(content, "id"), "01-introduction")
+  expect_equal(xml2::xml_attr(content, "id"), "introduction")
 
   # add an ephemeral section and write it out
   xml2::xml_add_sibling(content[[1]], "section", id = "ephemeral")
@@ -106,7 +106,7 @@ test_that("instructor-notes page can be rebuilt", {
   html <- xml2::read_html(inotes)
   content <- get_content(html, "section[@id='aggregate-instructor-notes']/section")
   expect_length(content, 1L)
-  expect_equal(xml2::xml_attr(content, "id"), "01-introduction")
+  expect_equal(xml2::xml_attr(content, "id"), "introduction")
   expect_match(xml2::xml_text(xml2::xml_find_first(content[[1]], ".//p")), 
     "Inline instructor notes")
 
@@ -168,7 +168,7 @@ test_that("single files can be built", {
   suppressMessages(s <- get_episodes(tmp))
   set_episodes(tmp, s, write = TRUE)
 
-  rdr <- sandpaper_site(fs::path(tmp, "episodes", "02-second-episode.Rmd"))
+  rdr <- sandpaper_site(fs::path(tmp, "episodes", "second-episode.Rmd"))
   expect_named(rdr, c("name", "output_dir", "render", "clean", "subdirs"))
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
@@ -176,7 +176,7 @@ test_that("single files can be built", {
   suppressMessages({
     rdr$render() %>%
       expect_output("ordinary text without R code") %>%
-      expect_message("Output created: .*02-second-episode.html")
+      expect_message("Output created: .*second-episode.html")
   })
 
 })
@@ -185,19 +185,19 @@ test_that("single files can be built", {
 test_that("Individual files contain matching metadata", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
-  idx <- xml2::read_html(fs::path(path_site(tmp), "docs", "02-second-episode.html"))
+  idx <- xml2::read_html(fs::path(path_site(tmp), "docs", "second-episode.html"))
 
   actual <- xml2::xml_find_first(idx, ".//script[@type='application/ld+json']")
   actual <- trimws(xml2::xml_text(actual))
   expect_match(actual, '"name": "Second Episode!"')
-  expect_match(actual, "02-second-episode.html")
+  expect_match(actual, "second-episode.html")
 })
 
 test_that("single files can be re-built", {
 
   skip_on_os("windows")
-  expect_hashed(tmp, "02-second-episode.Rmd")
-  rdr <- sandpaper_site(fs::path(tmp, "episodes", "02-second-episode.Rmd"))
+  expect_hashed(tmp, "second-episode.Rmd")
+  rdr <- sandpaper_site(fs::path(tmp, "episodes", "second-episode.Rmd"))
   expect_named(rdr, c("name", "output_dir", "render", "clean", "subdirs"))
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
@@ -205,7 +205,7 @@ test_that("single files can be re-built", {
   suppressMessages({
     rdr$render() %>%
       expect_output("ordinary text without R code") %>%
-      expect_message("Output created: .*02-second-episode.html")
+      expect_message("Output created: .*second-episode.html")
   })
 
   suppressMessages({
@@ -221,8 +221,8 @@ test_that("source files are hashed", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
   # see helper-hash.R
-  h1 <- expect_hashed(tmp, "01-introduction.Rmd")
-  h2 <- expect_hashed(tmp, "02-second-episode.Rmd")
+  h1 <- expect_hashed(tmp, "introduction.Rmd")
+  h2 <- expect_hashed(tmp, "second-episode.Rmd")
   # the hashes will no longer be equal because the titles are now different
   expect_failure(expect_equal(h1, h2, ignore_attr = TRUE))
 
@@ -231,10 +231,10 @@ test_that("source files are hashed", {
 test_that("HTML files are present and have the correct elements", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
-  expect_true(fs::file_exists(fs::path(sitepath, "01-introduction.html")))
-  expect_true(fs::file_exists(fs::path(sitepath, "02-second-episode.html")))
+  expect_true(fs::file_exists(fs::path(sitepath, "introduction.html")))
+  expect_true(fs::file_exists(fs::path(sitepath, "second-episode.html")))
   expect_true(fs::file_exists(fs::path(sitepath, "index.html")))
-  ep <- readLines(fs::path(sitepath, "01-introduction.html"))
+  ep <- readLines(fs::path(sitepath, "introduction.html"))
 
   # Div tags show up as expected
   expect_true(any(grepl(".div.+? class..callout challenge", ep)))
@@ -243,10 +243,10 @@ test_that("HTML files are present and have the correct elements", {
   expect_true(any(grepl("Sun arise each and every morning", ep)))
   expect_true(any(grepl(
         ".div.+? class..callout challenge", 
-        readLines(fs::path(sitepath, "02-second-episode.html"))
+        readLines(fs::path(sitepath, "second-episode.html"))
   )))
   expect_true(any(grepl(
-        "02-second-episode.html",
+        "second-episode.html",
         readLines(fs::path(sitepath, "index.html"))
   )))
 })
@@ -261,7 +261,7 @@ test_that("files will not be rebuilt unless they change in content", {
     })
   })
 
-  fs::file_touch(fs::path(tmp, "episodes", "01-introduction.Rmd"))
+  fs::file_touch(fs::path(tmp, "episodes", "introduction.Rmd"))
 
   suppressMessages({
     expect_failure({
@@ -270,8 +270,8 @@ test_that("files will not be rebuilt unless they change in content", {
     })
   })
 
-  expect_true(fs::file_exists(fs::path(sitepath, "01-introduction.html")))
-  expect_true(fs::file_exists(fs::path(sitepath, "02-second-episode.html")))
+  expect_true(fs::file_exists(fs::path(sitepath, "introduction.html")))
+  expect_true(fs::file_exists(fs::path(sitepath, "second-episode.html")))
 
 })
 
@@ -325,7 +325,7 @@ test_that("aio page is updated with new pages", {
     ".//div[contains(@class, 'lesson-content')]/section[starts-with(@id, 'aio-')]")
   expect_length(content, 2L)
   expect_equal(xml2::xml_attr(content, "id"), 
-    c("aio-01-introduction", "aio-02-second-episode"))
+    c("aio-introduction", "aio-second-episode"))
 
 })
 
@@ -346,22 +346,22 @@ test_that("episodes with HTML in the title are rendered correctly", {
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
 
-  se <- readLines(fs::path(tmp, "episodes", "02-second-episode.Rmd"))
+  se <- readLines(fs::path(tmp, "episodes", "second-episode.Rmd"))
   se[[2]] <- "title: A **bold** title"
-  writeLines(se, fs::path(tmp, "episodes", "02-second-episode.Rmd"))
+  writeLines(se, fs::path(tmp, "episodes", "second-episode.Rmd"))
 
   suppressMessages({
     expect_output(build_lesson(tmp, preview = FALSE, quiet = FALSE), "ordinary text without R code")
   })
 
-  h1 <- expect_hashed(tmp, "01-introduction.Rmd")
-  h2 <- expect_hashed(tmp, "02-second-episode.Rmd")
+  h1 <- expect_hashed(tmp, "introduction.Rmd")
+  h2 <- expect_hashed(tmp, "second-episode.Rmd")
   expect_failure(expect_equal(h1, h2, ignore_attr = TRUE))
-  expect_true(fs::file_exists(fs::path(sitepath, "02-second-episode.html")))
+  expect_true(fs::file_exists(fs::path(sitepath, "second-episode.html")))
 
   expect_true(any(grepl(
         "A <strong>bold</strong> title", 
-        readLines(fs::path(sitepath, "02-second-episode.html")),
+        readLines(fs::path(sitepath, "second-episode.html")),
         fixed = TRUE
   )))
 })
