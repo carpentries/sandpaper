82e82843d2500c4a585c38301e28ef1af2616d89
Zhian N. Kamvar <zkamvar@gmail.com>
2023-01-25 11:38:21 -0800

    add some noise to tests

--- tests/testthat/test-build_episode.R
+++ tests/testthat/test-build_episode.R
@@ -1,5 +1,7 @@
 res <- tmp <- restore_fixture()
 
+print(fs::dir_tree(res, recurse = 1))
+
 
 
 test_that("build_episode_html() returns nothing for an empty page", {
@@ -16,7 +18,7 @@ test_that("build_episode functions works independently", {
   withr::local_options(list(sandpaper.use_renv = FALSE))
   pkg <- pkgdown::as_pkgdown(file.path(tmp, "site"))
   expect_output(pkgdown::init_site(pkg))
-  
+
 
   skip_if_not(rmarkdown::pandoc_available("2.11"))
   # create a new file in extras
@@ -35,16 +37,16 @@ test_that("build_episode functions works independently", {
   expect_equal(lines[[2]], "title: Fun times")
   from_r <- grep("This is coming from", lines)
   expect_match(lines[from_r], "This is coming from R (version|Under)")
-  
+
   # Explicitly testing https://github.com/carpentries/sandpaper/issues/288
   # If we specify a `new.env()`, then the S3 dispatch will not work, but when we
-  # default to `globalenv()`, the S3 dispatch works. 
+  # default to `globalenv()`, the S3 dispatch works.
   expect_false(any(grepl("Error", lines)))
 
   expect_false(file.exists(file.path(tmp, "site", "docs", "fun.html")))
   expect_false(file.exists(file.path(tmp, "site", "docs", "instructor", "fun.html")))
   expect_output({
-    build_episode_html(res, 
+    build_episode_html(res,
       fun_file,
       page_back = fun_file,
       page_forward = fun_file,
@@ -73,20 +75,20 @@ test_that("the chapter-links should be cromulent depending on the view", {
   expect_length(learn_links, 4L)
   expect_equal(xml2::xml_attr(learn_links, "href"), rep("fun.html", 4L))
 
-  learn_note<- xml2::xml_find_all(learn, 
+  learn_note<- xml2::xml_find_all(learn,
     ".//div[starts-with(@id, 'accordionInstructor')]")
   expect_length(learn_note, 0L)
 
   instruct_links <- xml2::xml_find_all(instruct, ".//a[@class='chapter-link']")
   expect_length(instruct_links, 4L)
-  expect_equal(xml2::xml_attr(instruct_links, "href"), 
+  expect_equal(xml2::xml_attr(instruct_links, "href"),
     rep("../instructor/fun.html", 4L))
 
   internal_instruct_links <- xml2::xml_find_all(instruct, ".//a[contains(text(), 'internal')]")
   expect_length(internal_instruct_links, 1L)
   expect_equal(xml2::xml_attr(internal_instruct_links, "href"), "fun.html")
 
-  instruct_note <- xml2::xml_find_all(instruct, 
+  instruct_note <- xml2::xml_find_all(instruct,
     ".//div[starts-with(@id, 'accordionInstructor')]")
   expect_length(instruct_note, 1L)
   IN_lines <- trimws(xml2::xml_text(instruct_note))
