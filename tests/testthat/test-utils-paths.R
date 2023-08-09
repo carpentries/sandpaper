3a3695e2117f29391e448a3a2b7ea717965f6568
Zhian N. Kamvar <zkamvar@gmail.com>
2023-08-09 09:01:10 -0700

    fix mac issue

--- tests/testthat/test-utils-paths.R
+++ tests/testthat/test-utils-paths.R
@@ -3,24 +3,26 @@ res <- restore_fixture()
 
 test_that("root path will find the root of the lesson locally", {
 
-  here  <- as.character(res)
-  expect_identical(root_path(here), here)
+  here  <- fs::path_file(as.character(res))
+
+  expect_identical(fs::path_file(root_path(res)), here)
+
 
   there <- fs::path(res, "episodes")
-  expect_identical(root_path(there), here)
+  expect_identical(fs::path_file(root_path(there)), here)
 
   there <- fs::path(res, "learners", "setup.md")
-  expect_identical(root_path(there), here)
+  expect_identical(fs::path_file(root_path(there)), here)
 
   # if we insert a config.yaml file into the built folder, it will not cause a
   # problem
   there <- fs::path(res, "site", "built")
   fs::file_copy(fs::path(res, "config.yaml"), there)
-  expect_identical(root_path(there), here)
+  expect_identical(fs::path_file(root_path(there)), here)
 
   # removing the episodes folder does not invalidate the lesson
-  fs::dir_delete(fs::path(here, "episodes"))
-  expect_identical(root_path(there), here)
+  fs::dir_delete(fs::path(res, "episodes"))
+  expect_identical(fs::path_file(root_path(there)), here)
 
 })
 
