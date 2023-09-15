a2c79ac56a1a5f72650d61338cd9b9be20c5ff38
Zhian N. Kamvar <zkamvar@gmail.com>
2023-09-15 07:38:03 -0700

    reformat test code

--- tests/testthat/test-utils-built-db.R
+++ tests/testthat/test-utils-built-db.R
@@ -3,7 +3,8 @@ res <- restore_fixture()
 
 test_that("get_child_files() will return a list equal to the files in the lesson", {
   lsn <- this_lesson(res)
-  expected <- lapply(c(lsn$get("path", c("episodes", "extra"))),
+  expected <- lapply(
+    c(lsn$get("path", c("episodes", "extra"))),
     function(p) fs::path_abs(p, start = res)
   )
   names(expected) <- vapply(expected,
@@ -21,7 +22,6 @@ test_that("get_child_files() will return a list equal to the files in the lesson
 
 
 test_that("build_status() will record new entries", {
-
   outdir <- path_built(res)
   db_path <- fs::path(outdir, "md5sum.txt")
   # database should not exist
@@ -65,13 +65,10 @@ test_that("build_status() will record new entries", {
   expect_type(stat$build, "character")
   expect_s3_class(stat$new, "data.frame")
   expect_named(stat$new, c("file", "checksum", "built", "date"))
-
-
 })
 
 
 test_that("build_status() will return no differences if no files change", {
-
   outdir <- path_built(res)
   db_path <- fs::path(outdir, "md5sum.txt")
   # database should not exist
@@ -113,19 +110,18 @@ test_that("build_status() will return no differences if no files change", {
   expect_named(restat$new, c("file", "checksum", "built", "date"))
   expect_named(restat$old, c("file", "checksum", "built", "date"))
   expect_equal(restat$checksum, restat$checksum)
-
 })
 
 
 
 test_that("get_child_files() will return a list of files that have child documents in lessons", {
-
   # setup our test and then burn it down
   files <- setup_child_test(res)
   withr::defer(fs::file_delete(files))
 
   lsn <- this_lesson(res)
-  expected <- lapply(c(lsn$get("path", c("episodes", "extra"))),
+  expected <- lapply(
+    c(lsn$get("path", c("episodes", "extra"))),
     function(p) fs::path_abs(p, start = res)
   )
   expected[["child-haver.Rmd"]] <- c(
@@ -141,13 +137,11 @@ test_that("get_child_files() will return a list of files that have child documen
   )
   expect_type(get_child_files(lsn), "list")
   expect_equal(get_child_files(lsn), expected)
-
 })
 
 
 
 test_that("build_status() takes into account child document modifications", {
-
   # setup our test and then burn it down
   files <- setup_child_test(res)
   withr::defer(fs::file_delete(files))
@@ -237,10 +231,5 @@ test_that("build_status() takes into account child document modifications", {
   # no files are to be removed
   expect_type(restat$remove, "character")
   expect_length(restat$remove, 0L)
-
 })
-
-
-
-
 
