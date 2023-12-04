e965a5ab74f5a6ceadefd8d3ae3d084f73aaad2f
Zhian N. Kamvar <zkamvar@gmail.com>
2023-12-04 12:14:20 -0800

    add test for aria menu items

--- tests/testthat/test-translate.R
+++ tests/testthat/test-translate.R
@@ -11,12 +11,14 @@ test_that("Lessons can be translated with lang setting", {
   skip_if_not(rmarkdown::pandoc_available("2.11"))
 
   # Build lesson
-  suppressMessages(build_lesson(tmp, preview = FALSE, quiet = FALSE))
+  suppressMessages(build_lesson(tmp, preview = FALSE, quiet = TRUE))
 
   # Extract first header (Summary and Setup) from index
-  index <- readLines(fs::path(sitepath, "index.html"))
-  xml <- xml2::read_html(paste(index, collapse = ""))
+  xml <- xml2::read_html(fs::path(sitepath, "index.html"))
   h1_header <- xml2::xml_find_all(xml, "//h1[@class='schedule-heading']")
+
+  # language should be set to japanese
+  expect_equal(xml2::xml_attr(xml, "lang"), "ja")
 
   # Header should be translated to Japanese
   expect_true(
@@ -33,5 +35,18 @@ test_that("Lessons can be translated with lang setting", {
       withr::with_language("en", tr_("Summary and Setup"))
     )
   )
+
+  # aria labels should be translated
+  arias <- c("Main Navigation", "Toggle Navigation", "Search", "search button",
+    "Lesson Progress", "close menu", "Next Chapter", "anchor", "Back To Top")
+  ja_arias <- withr::with_language("ja", vapply(arias, tr_, character(1)))
+
+  expect_false(identical(arias, ja_arias))
+
+  expect_setequal(
+    ja_arias,
+    xml2::xml_text(xml2::xml_find_all(xml, ".//@aria-label"))
+  )
+
 
 })
