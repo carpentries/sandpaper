11e66b05e17baccf904df04a9b77c2e10dc4ea7c
froggleston <robertdavey@carpentries.org>
2025-10-10 22:39:08 +0100

    Add support for parsing CITATION.cff

--- R/build_citation.R
+++ R/build_citation.R
@@ -6,8 +6,11 @@ parse_cff <- function(cff_file, lesson) {
   }
 
   authors <- cff_data$authors %||% NULL
+  affiliations <- ""
   if (!is.null(authors)) {
-    authors <- parse_authors(authors)
+    auth_env <- parse_authors(authors)
+    authors <- auth_env$authors
+    affiliations <- auth_env$affiliations
   }
   title <- cff_data$title %||% lesson$title %||% "Untitled Lesson"
   abstract <- cff_data$abstract %||% lesson$description %||% ""
@@ -25,8 +28,10 @@ parse_cff <- function(cff_file, lesson) {
 
   url <- cff_data$url %||% lesson$url %||% NULL
 
-  citation <- paste0(authors, " (", format(as.Date(date_released), "%Y"), "). ",
-                     title, ". Version ", version, ".")
+  citation <- paste0(authors, " (", format(as.Date(date_released), "%Y"), "). <em>",
+                     title, "</em>. Version ", version, ".")
+
+  citation <- paste0(citation, affiliations)
 
   # citation <- paste("<ul>")
   doi <- cff_data$doi %||% NULL
@@ -46,15 +51,18 @@ parse_cff <- function(cff_file, lesson) {
   cf_pc <- cff_data$`preferred-citation` %||% NULL
   if (!is.null(cf_pc)) {
     pc_authors <- cf_pc$authors %||% NULL
-    if (is.null(pc_authors)) {
-      pc_authors <- parse_authors(pc_authors)
+    pc_affiliations <- ""
+    if (!is.null(pc_authors)) {
+      pc_auth_env <- parse_authors(pc_authors)
+      pc_authors <- pc_auth_env$authors
+      pc_affiliations <- pc_auth_env$affiliations
     }
     pc_type <- cf_pc$type %||% NULL
     pc_version <- cf_pc$version %||% version
     if (pc_type == "article") {
       pc_journal <- cf_pc$journal %||% ""
-      citation <- paste0(pc_authors, " (", cf_pc$year, "). ",
-                         cf_pc$title, ". ", pc_journal)
+      citation <- paste0(pc_authors, " (", cf_pc$year, "). <em>",
+                         cf_pc$title, "</em>. ", pc_journal)
       if (!is.null(cf_pc$volume)) {
         citation <- paste0(citation, ", ", cf_pc$volume)
       }
@@ -69,14 +77,16 @@ parse_cff <- function(cff_file, lesson) {
       }
     } else {
       # Fallback to preferred citation details if type is not article
-      citation <- paste0(pc_authors, " (", cf_pc$year, "). ",
-                         pc_title, ". Version [", pc_version, "].")
+      citation <- paste0(pc_authors, " (", cf_pc$year, "). <em>",
+                         pc_title, "</em>. Version [", pc_version, "].")
       if (!is.null(pc_doi)) {
         citation <- paste0(citation, " https://doi.org/", pc_doi)
       } else if (!is.null(url)) {
         citation <- paste0(citation, " ", url)
       }
     }
+
+    citation <- paste0(citation, pc_affiliations)
   }
 
   return(citation)
@@ -145,7 +155,7 @@ build_citation <- function(pkg, quiet = FALSE) {
   }
   fix_nodes(html)
 
-  ct <- parse_cff(this_metadata$get()$cff)
+  ct <- xml2::read_html(parse_cff(this_metadata$get()$cff))
   this_metadata$set("citation", ct)
 
   this_dat <- list(
@@ -157,15 +167,40 @@ build_citation <- function(pkg, quiet = FALSE) {
   page_globals$learner$update(this_dat)
   page_globals$metadata$update(this_dat)
 
+  outpath <- fs::path(pkg$dst_path, "citation.html")
+  inst_outpath <- fs::path(pkg$dst_path, "instructor/citation.html")
+  out <- fs::path_rel(inst_outpath, pkg$dst_path)
+
+  already_built <- template_check$valid() && fs::file_exists(inst_outpath)
+
+  if (already_built) {
+    report <- "Rebuilding '{.file {out}}'"
+    if (!quiet) cli::cli_text(report)
+
+    # delete the existing instructors/citation.html file
+    fs::file_delete(inst_outpath)
+
+    built_path <- fs::path(pkg$src_path, "built")
+  }
+  else {
+    report <- "Appending to '{.file {out}}'"
+    if (!quiet) cli::cli_text(report)
+  }
+
   build_html(template = "extra", pkg = pkg, nodes = html,
     global_data = page_globals, path_md = "citation.html", quiet = quiet)
 
-  outpath <- fs::path(pkg$dst_path, "citation.html")
   ref_html <- xml2::read_html(outpath)
-  ref_sect <- xml2::xml_find_first(ref_html, ".//section/h2[@id='cite-this-lesson']")
+  ref_sect <- xml2::xml_find_first(ref_html, ".//section/h2[@id='cite-this-lesson']/parent::section")
   cit_sect <- xml2::xml_add_child(ref_sect, "p", id="citation")
-  xml2::xml_set_text(cit_sect, as.character(ct))
+  xml2::xml_add_child(cit_sect, ct)
   writeLines(as.character(ref_html), outpath)
+
+  ref_html <- xml2::read_html(inst_outpath)
+  ref_sect <- xml2::xml_find_first(ref_html, ".//section/h2[@id='cite-this-lesson']/parent::section")
+  cit_sect <- xml2::xml_add_child(ref_sect, "p", id="citation")
+  xml2::xml_add_child(cit_sect, ct)
+  writeLines(as.character(ref_html), inst_outpath)
 }
 
 # Function to process CFF author list
@@ -174,22 +209,58 @@ parse_authors <- function(authors) {
     return("Unknown Author")
   }
 
+  env <- new.env()
+  env$unique_affiliations <- c()
+
   author_names <- sapply(authors, function(author) {
     if (is.list(author)) {
       given <- author[["given-names"]] %||% ""
       family <- author[["family-names"]] %||% ""
-      paste(given, family) |> stringr::str_trim()
+      affiliation <- author[["affiliation"]] %||% ""
+      orcid <- author[["orcid"]] %||% ""
+      name <- paste(given, family) |> stringr::str_trim()
+
+      affil_num <- ""
+      # add affiliation to unique list for use as links
+      if (affiliation != "") {
+        env$unique_affiliations <- union(env$unique_affiliations, c(affiliation))
+        affil_num <- which(env$unique_affiliations == affiliation)[[1]] %||% ""
+      }
+
+      # add superscript number for affiliation and orcid if available
+      name <- paste0(name, "<span class='author-info'>")
+      if (affil_num != "") {
+        name <- paste0(name, " <a href='#aff", affil_num, "'><sup>", affil_num, "</sup></a> ")
+      }
+      if (orcid != "") {
+        name <- paste0(name, " <a href='", orcid, "' target='_blank'><sup><img src='assets/images/orcid_icon.png' height='12' width='12'/></sup></a>")
+      }
+      name <- paste0(name, "</span>")
     } else {
       as.character(author)
     }
   })
 
   if (length(author_names) == 1) {
-    return(author_names[1])
+    output <- author_names[1]
   } else if (length(author_names) == 2) {
-    return(paste(author_names, collapse = " and "))
+    output <- paste(author_names, collapse = " and ")
   } else {
-    return(paste(paste(author_names[-length(author_names)], collapse = ", "),
-                 "and", author_names[length(author_names)]))
+    output <- paste(paste(author_names[-length(author_names)], collapse = ", "),
+                 "and", author_names[length(author_names)])
   }
+
+  # generate a numbered list of affiliations
+  affil_list <- ""
+  if (length(env$unique_affiliations) > 0) {
+    affil_list <- "<p style='font-size=0.8em'>Affiliations:<br/><ol class='affiliations'>"
+    for (i in seq_along(env$unique_affiliations)) {
+      affil_list <- paste0(affil_list, "<li id='aff", i, "'>", env$unique_affiliations[i], "</li>")
+    }
+    affil_list <- paste0(affil_list, "</ol></p>")
+  }
+
+  env$authors <- output
+  env$affiliations <- affil_list
+  return(env)
 }
